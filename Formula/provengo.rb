d0e0c1764948d5c70c394f39e007d882ed0726af
Michael Bar-Sinai <mich.barsinai@gmail.com>
2025-07-14 01:06:50 -0400

    Improved layout

--- Formula/provengo.rb
+++ Formula/provengo.rb
@@ -1,8 +1,8 @@
 class Provengo < Formula
   desc "Scenario-based modeling and testing tool"
   homepage "https://www.provengo.tech/"
-  url "https://downloads.provengo.tech/releases/Provengo-2025-07-04.uber.jar"
-  sha256 "dcb0ead49f8755346e322acce0424bb4c4ac33ab5ac9ee0eb8c363537f27cbbc"
+  url "https://downloads.provengo.tech/releases/Provengo-2025-07-14.uber.jar"
+  sha256 "5a8140409e200fb752a97b7317e1b259e668383d9d42814063b3365f0044981b"
 
   depends_on "graphviz"
 
@@ -20,14 +20,14 @@ class Provengo < Formula
   def install
     check_java_version
 
-    libexec.install "Provengo-2025-07-04.uber.jar"
+    libexec.install "Provengo-2025-07-14.uber.jar"
     (bin/"provengo").write <<~EOS
       #!/bin/bash
       JAVA_VERSION=$(java --version | head -n1 | awk '{ print $2 }' | cut -d. -f1)
       if (( $JAVA_VERSION > 23 )); then
           SWITCH=--enable-native-access=ALL-UNNAMED
       fi
-      exec java $SWITCH -jar "#{libexec}/Provengo-2025-07-04.uber.jar" "$@"
+      exec java $SWITCH -jar "#{libexec}/Provengo-2025-07-14.uber.jar" "$@"
     EOS
   end
 
